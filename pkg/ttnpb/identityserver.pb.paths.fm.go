nejraselimovic/lorawan-stack-1

dev: Update api for user session authentication

pkg/ttnpb/identityserver.pb.paths.fm.go
@@ -44,6 +44,16 @@ var AuthInfoResponseFieldPathsNested = []string{
 	"access_method.oauth_access_token.user_ids.email",
 	"access_method.oauth_access_token.user_ids.user_id",
 	"access_method.oauth_access_token.user_session_id",
+	"access_method.user_session",
+	"access_method.user_session.created_at",
+	"access_method.user_session.expires_at",
+	"access_method.user_session.session_id",
+	"access_method.user_session.token_id",
+	"access_method.user_session.token_key",
+	"access_method.user_session.updated_at",
+	"access_method.user_session.user_ids",
+	"access_method.user_session.user_ids.email",
+	"access_method.user_session.user_ids.user_id",
 	"is_admin",
 	"universal_rights",
 	"universal_rights.rights",

shhlyhq/lorawan-stack

pkg/ttnpb/identityserver.pb.paths.fm.go
@@ -44,6 +44,15 @@ var AuthInfoResponseFieldPathsNested = []string{
 	"access_method.oauth_access_token.user_ids.email",
 	"access_method.oauth_access_token.user_ids.user_id",
 	"access_method.oauth_access_token.user_session_id",
+	"access_method.user_session",
+	"access_method.user_session.created_at",
+	"access_method.user_session.expires_at",
+	"access_method.user_session.session_id",
+	"access_method.user_session.session_secret",
+	"access_method.user_session.updated_at",
+	"access_method.user_session.user_ids",
+	"access_method.user_session.user_ids.email",
+	"access_method.user_session.user_ids.user_id",
 	"is_admin",
 	"universal_rights",
 	"universal_rights.rights",